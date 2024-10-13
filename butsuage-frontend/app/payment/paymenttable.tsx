8f3e3732fd7b7c5660973bc7be5e4c263309e4f7
kouta222 <kouta9369@gmail.com>
2024-10-13 17:44:40 +0900

    payment

---
+++ butsuage-frontend/app/payment/paymenttable.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+function PricingPage() {
+  // Paste the stripe-pricing-table snippet in your React component
+  return <stripe-pricing-table pricing-table-id="prctbl_1Q9MIh2NBFGUETwJ8JZzY8tV" publishable-key="pk_test_51Pzs6W2NBFGUETwJgR5hG2IGwftHgAS6Ww0OiITXl1ddS3NYHFf5qU7yPYLUY5pkJ4FpUkeYyyDLq4YvBpYJNEiF00fqvrV7WU"></stripe-pricing-table>;
+}
+
+export default PricingPage;
